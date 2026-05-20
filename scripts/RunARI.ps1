91444571a1f3572b6e69fd107710c998761518f8
Yogesh <yogikhairnar1912@gmail.com>
2026-05-20 07:34:09 +0000

    Initial ARI automation

---
+++ scripts/RunARI.ps1
@@ -0,0 +1,43 @@
+# =====================================
+# Azure Resource Inventory Automation
+# =====================================
+
+# Service Principal Login
+
+$AppId = $env:AZURE_CLIENT_ID
+$Password = $env:AZURE_CLIENT_SECRET
+$TenantId = $env:AZURE_TENANT_ID
+
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+$Credential = New-Object System.Management.Automation.PSCredential($AppId, $SecurePassword)
+
+Connect-AzAccount `
+-ServicePrincipal `
+-Tenant $TenantId `
+-Credential $Credential
+
+# Create Reports Folder
+
+$ReportFolder = "./reports"
+
+if (!(Test-Path $ReportFolder)) {
+    New-Item -ItemType Directory -Path $ReportFolder
+}
+
+# Date
+
+$Date = Get-Date -Format "yyyyMMdd_HHmmss"
+
+# Import ARI
+
+Import-Module AzureResourceInventory
+
+# Run ARI
+
+Invoke-ARI `
+-TenantID $TenantId `
+-ReportDir $ReportFolder `
+-ReportName "AzureInventory_$Date"
+
+Write-Host "ARI Report Generated"
